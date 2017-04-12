5908c76a5f95ab14f54c3c889023500e136221e6
ruturajp <ruturajp@sfu.ca>
2017-04-11 22:19:16 -0700

    UI Files added

--- templates/assets/js/filemanagement/fileupload.js
+++ templates/assets/js/filemanagement/fileupload.js
@@ -1,61 +1,83 @@
-/**
- * Created by jay on 02/04/17.
- */
 
 $(document).ready(function () {
 
-    $(".upload-file").change(function () {
+
+    $('#input_prog_file').on("change", function(){ 
         var file = this.files[0];
+        var flag = false;
         name = file.name;
         size = file.size;
         type = file.type;
-
+        var extension = name.replace(/^.*\./, '');
         if (file.name.length < 1) {
-            alert("Please select a file")
+            alert("Please select a file");
+            flag = true;
+
         }
         else if (file.size > 100000) {
             alert("The file is too big");
+            flag=true;
+
         }
-        // else if(file.type != 'image/png' && file.type != 'image/jpg' && file.type != 'image/gif' && file.type != 'image/jpeg' ) {
-        //     alert("The file does not match png, jpg or gif");
-        // }
+        else if(extension != 'py' ) {
+            alert("We only support zip files for now");
+            flag = true;
+
+        }
         else {
             $('.btnFile').attr("disabled", false);
 
+        }
+
+        if(flag == true){
+            $(this).val('');
+        }
+    });
+
+    $('#input_data_file').on("change", function(){ 
+        var file = this.files[0];
+        var flag = false;
+        name = file.name;
+        size = file.size;
+        type = file.type;
+        var extension = name.replace(/^.*\./, '');
+        if (file.name.length < 1) {
+            alert("Please select a file");
+            flag = true;
+
+        }
+        
+        else if(extension != 'zip' ) {
+            alert("We only support zip files for now");
+            flag = true;
+
+        }
+        else {
+            $('.btnFile').attr("disabled", false);
+
+        }
+
+        if(flag == true){
+            $(this).val('');
         }
     });
 
     $('.btnFile').click(function (e) {
                 e.preventDefault();
 
-                var formData = new FormData($('#form1')[0]);
+                var formData = new FormData($('#forminput')[0]);
                 $.ajax({
                     url: 'api/uploadcode',
                     type: 'POST',
-                    // xhr: function() {  // custom xhr
-                    //     myXhr = $.ajaxSettings.xhr();
-                    //     if(myXhr.upload){ // if upload property exists
-                    //         myXhr.upload.addEventListener('progress', progressHandlingFunction, false); // progressbar
-                    //     }
-                    //     return myXhr;
-                    // },
-                    // Ajax events
+                    
                     success: completeHandler = function (data) {
-                        // /*
-                        // * Workaround for Chrome browser // Delete the fake path
-                        // */
-                        // if(navigator.userAgent.indexOf('Chrome')) {
-                        //     var catchFile = $(":file").val().replace(/C:\\fakepath\\/i, '');
-                        // }
-                        // else {
-                        //     var catchFile = $(":file").val();
-                        // }
-                        // var writeFile = $(":file");
-                        // writeFile.html(writer(catchFile));
-                        $(".upload-status").html("File Uploaded");
+                      
+                        $(".upload-prog-status").html("File Uploaded");
+                        $(".upload-prog-status").css('color','green');
                     },
                     error: errorHandler = function (error1) {
-                        $(".upload-status").html("File Upload failed");
+                        $(".upload-prog-status").html("File Upload failed");
+                        $(".upload-prog-status").css('color','red');
                     },
                     // Form data
                     data: formData,
@@ -68,36 +90,22 @@ $(document).ready(function () {
                 return false;
     });
 
-    $(".upload-data-file").change(function () {
-        var file = this.files[0];
-        name = file.name;
-        size = file.size;
-        type = file.type;
-
-        if (file.name.length < 1) {
-            alert("Please select a file")
-        }
-        else if (file.size > 100000) {
-            alert("The file is too big");
-        }
-        else {
-            $('.btnDataFile').attr("disabled", false);
-        }
-    });
-
     $('.btnDataFile').click(function (e) {
         e.preventDefault();
         debugger;
-        var formData = new FormData($('#form1')[0]);
+        var formData = new FormData($('#formdatainput')[0]);
         $.ajax({
             url: 'api/uploaddata',
             type: 'POST',
             success: completeHandler = function (data) {
-                $(".upload-status").html("File Uploaded");
-            },
-            error: errorHandler = function (error1) {
-                $(".upload-status").html("File Upload failed");
-            },
+                      
+                        $(".upload-data-status").html("File Uploaded");
+                        $(".upload-data-status").css('color','green');
+                    },
+                    error: errorHandler = function (error1) {
+                        $(".upload-data-status").html("File Upload failed");
+                        $(".upload-data-status").css('color','red');
+                    },
             // Form data
             data: formData,
             // Options to tell jQuery not to process data or worry about the content-type
